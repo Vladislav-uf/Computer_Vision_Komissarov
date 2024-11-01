37a9b39ea3116750488f0383782e1f7fc74a1157
VLadislav-uf <aydan.kalitva@gmail.com>
2024-11-01 12:19:40 +0000

    unlucky

---
+++ src/main/java/ru/sfedu/opencv/constants/Constants.java
@@ -0,0 +1,44 @@
+package ru.sfedu.opencv.constants;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.opencv.imgproc.Imgproc;
+
+public class Constants {
+    public static final String PATH_TO_NATIVE_LIB_LINUX = "cv.file.path";
+
+    public static final String LAB2_OUTPUT_PATH = "lab2.output.path";
+    public static final String LAB2_IMAGE_PATH = "lab2.image.path";
+
+    public static final String LAB3_OUTPUT_PATH = "lab3.output.path";
+    public static final String LAB3_IMAGE_PATH = "lab3.image.path";
+    public static final String LAB3_IMAGE2_PATH = "lab3.image2.path";
+
+    public static final String LAB4_OUTPUT_PATH = "lab4.output.path";
+    public static final String LAB4_IMAGE_PATH = "lab4.image.path";
+
+    public static final String LAB5_OUTPUT_PATH = "lab5.output.path";
+    public static final String LAB5_IMAGE_TO_FLOOD_PATH = "lab5.image-to-flood.path";
+    public static final String LAB5_IMAGE_TO_PUR_PATH = "lab5.image-to-pur.path";
+    public static final String LAB5_IMAGE_TO_FIND_RECT_PATH = "lab5.image-to-find-rect.path";
+
+    public static final String LAB6_OUTPUT_PATH = "lab6.output.path";
+    public static final String LAB6_IMAGE_TO_DEFINE_BORDER_PATH = "lab6.image-to-define-border.path";
+    public static final String LAB6_IMAGE_TO_DEFINE_BORDER2_PATH = "lab6.image-to-define-border2.path";
+
+    public enum OSType {
+        MACOS, WINDOWS, LINUX, OTHER
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum MORPH {
+        CROSS(Imgproc.MORPH_CROSS),
+        RECT(Imgproc.MORPH_RECT),
+        ELLIPSE(Imgproc.MORPH_ELLIPSE),
+        ;
+
+        private final int shape;
+
+    }
+}
